9bdf088e8413328eb34b26b567441948ace9c24d
GeethaKS <GeethaKS@SGEETHA-P14S>
2024-08-28 10:38:33 +0530

    Add project files.

---
+++ Data/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().ToTable("Todos");
+        }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }  // e.g., Pending, Completed
+        public DateTime CreatedOn { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
